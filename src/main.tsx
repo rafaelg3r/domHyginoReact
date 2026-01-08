37fac62ac47129231e5d82dc5147056ae8fc40e3
Rafael C. Vedana <rafael.dvedana@gmail.com>
2026-01-08 14:51:46 -0300

    fix: add base config on vite.config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import "./styles/global.css";
+import "./global.css";
 
 import { AppRouter } from "./routes/appRouter";
 import { Navbar } from "./components/Navbar";
